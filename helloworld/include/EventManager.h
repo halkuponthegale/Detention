ddbd79331ee591dee9dcf59767d12663ac01542a
Saltine17 <kyle.saltzberg@gmail.com>
2017-10-24 11:42:02 -0400

    Create EventManager.h

---
+++ helloworld/include/EventManager.h
@@ -0,0 +1,21 @@
+#include <map>
+typedef void(*evCallback)(void);
+class EventManager {
+public:
+	EventManager() {
+
+	}
+	void triggerEvent(std::string name) {
+		for (auto const& x : events) {
+			if (!(x.first.compare(name))) {
+				x.second();
+			}
+		}
+	}
+	void registerEvent(std::string name, evCallback callback) {
+		events.insert(std::pair<std::string,evCallback>(name,callback));
+	}
+
+private:
+	std::map<std::string, void(*)(void)> events;
+};
