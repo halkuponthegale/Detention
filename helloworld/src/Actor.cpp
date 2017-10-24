bb0c820b319b4d60bbc5de7c71b52f2569b75ab7
aminnick <33011039+aminnick@users.noreply.github.com>
2017-10-24 19:44:24 -0400

    #includes and comments

--- helloworld/src/Actor.cpp
+++ helloworld/src/Actor.cpp
@@ -1,1 +1,10 @@
+#include <SFML/Graphics.hpp>
+#include "Actor.h"
 
+// Subclasses:
+// 1. Player
+// 2. NPC (Hall Monitor, Teacher, Janitor, Principal)
+// 3. Machine (Mobile, Launcher, Shooter, Builder)
+// 4. Object ( Button, Lever, Wall)
+
+// TODO: 
