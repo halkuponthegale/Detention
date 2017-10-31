cc759db47d5059322bb917ae3cd137af5e94e747
aminnick <33011039+aminnick@users.noreply.github.com>
2017-10-30 23:31:36 -0400

    Implemented left and right movement

--- helloworld/src/Player.cpp
+++ helloworld/src/Player.cpp
@@ -14,7 +14,7 @@ sf::RectangleShape player_paddle;
     // Moves the player up, unless it's already at the top of the screen
     void player::player_up() {
         if(player_paddle.getPosition().y > 0) {
-            player_paddle.move(0, -0.07);
+            player_paddle.move(0, -1);
         }
     }
 
@@ -22,9 +22,26 @@ sf::RectangleShape player_paddle;
     // Moves the player down, unless it's already at the bottom of the screen
     void player::player_down() {
         if(player_paddle.getPosition().y < 570) {
-            player_paddle.move(0, 0.07);
+            player_paddle.move(0, 1);
         }
     }
+
+
+    // Moves the player left, unless it's already at the left of the screen
+    void player::player_left() {
+        if(player_paddle.getPosition().x < 0) {  // check
+            player_paddle.move(-1, 0);  // check
+        }
+    }
+
+
+    // Moves the player right, unless it's already at the right of the screen
+    void player::player_right() {
+        if(player_paddle.getPosition().x < 750) {  // check
+            player_paddle.move(1, 0);  // check
+        }
+    }
+
 
     // Puts paddle back to original position
     void player::reset() {
