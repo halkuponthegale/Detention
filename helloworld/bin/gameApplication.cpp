505e5766afc75927db8725bede321ccbbd001b19
aminnick <33011039+aminnick@users.noreply.github.com>
2017-10-31 00:03:12 -0400

    Implemented moving player, controlled by <>^v

--- helloworld/bin/gameApplication.cpp
+++ helloworld/bin/gameApplication.cpp
@@ -2,10 +2,12 @@
 
 #include "View.h"
 #include "MenuView.h"
+#include "Player.h"
 
 // push game display onto play_state, keep main loop clean
 View game_view;
 int top_lvl;
+player PlayerInstance;
 
 
 int main(int argc, char** argv)
@@ -29,9 +31,33 @@ int main(int argc, char** argv)
             if(Event.type == sf::Event::Closed)
             App.close();
         }
+        
+         // If user presses up or down, move the player paddle
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+                    PlayerInstance.player_up();
+            }
+
+            else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+                    PlayerInstance.player_down();
+            }
+        
+            else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                   PlayerInstance.player_left();
+            }
+        
+            else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                    PlayerInstance.player_right();
+            }
+
+            else { // If they press something other than up or down, don't do anything
+            }
+
 
         // clear screen and fill with black
         App.clear(sf::Color::Black);
+        
+        // Draw player
+        App.draw(playerInstance.playerbody);
 
         game_view.Update();
         game_view.Render();
@@ -40,7 +66,7 @@ int main(int argc, char** argv)
         // display
         App.display();
 
-
+    
     }
 
     // Done.
