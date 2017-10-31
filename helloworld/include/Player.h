bb761283c07111fb3fcacc76b7252dcba7e9752b
aminnick <33011039+aminnick@users.noreply.github.com>
2017-10-30 22:43:17 -0400

    basic movement update

--- helloworld/include/Player.h
+++ helloworld/include/Player.h
@@ -1,1 +1,27 @@
+#ifndef PLAYER_h
+#define PLAYER_h
+
+#include <SFML/Graphics.hpp>
+
+class player {
+
+public:
+
+    //Create a simple rectangle, stand in for the player
+    sf::RectangleShape player_paddle;
+
+    player(); // Constructor
+
+
+    // Moves the player up
+    void player_up();
+
+    // Moves the player down
+    void player_down();
+
+    // Puts player back in original position
+    void reset();
+
+};
+#endif
 
