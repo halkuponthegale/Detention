769d228eb0f58d0d8051352b8bd3ca7dde86244b
Saltine17 <kyle.saltzberg@gmail.com>
2017-10-31 16:58:24 -0400

    Update Actor.h

--- helloworld/include/Actor.h
+++ helloworld/include/Actor.h
@@ -1,1 +1,33 @@
-
+#pragma once
+class Actor{
+public:
+  Actor(double x, double y, double w, double h){
+    this->x = x;
+    this->y=y;
+    this->w=w;
+    this->h=h;
+  }
+  double getX(){
+    return x;
+  }
+  double getY(){
+    return y;
+  }
+  void setLocation(double x, double y){
+    this->x=x;
+    this->y=y;
+  }
+  double move(double dx, double dy){
+    x+=dx;
+    y+=dy;
+  }
+  int getW(){
+    return w;
+  }
+  int getH(){
+    return h;
+  }
+private:
+  double x,y;
+  int w,h;
+};
