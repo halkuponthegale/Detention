ce176015c1fcc1572dc9440ee9b22c598cc78914
Saltine17 <kyle.saltzberg@gmail.com>
2017-10-31 16:57:52 -0400

    Update Object.h

--- helloworld/include/Object.h
+++ helloworld/include/Object.h
@@ -1,1 +1,16 @@
+#pragma once
+#include "Actor.h"
+class Object: public Actor{
+public:
+  Object(double x, double y, int w, int h, bool solid) : Actor(x,y,w,h){
+    this->solid = solid;
+  }
+  void interact(){
 
+  }
+  bool isSolid(){
+    return solid;
+  }
+private:
+  bool solid;
+};
