f55f3de51b28385c3fb7d5d33b643c6aab8cf087
Saltine17 <kyle.saltzberg@gmail.com>
2017-11-07 19:50:21 -0500

    Update Level.h

--- helloworld/include/Level.h
+++ helloworld/include/Level.h
@@ -3,11 +3,15 @@
 #include "Object.h"
 class Level{
 public:
-  Level(std::vector<Object> objs){
-    levelObjects = objs;
+  Level(std::vector<std::unique_ptr<Actor>> walls, std::vector<std::unique_ptr<Actor>> machines, std::vector<std::unique_ptr<Actor>> objects){
+    this->walls = walls;
+    this->machines = machines;
+    this->objects = objects;
   }
   Level(std::string file){
     //parse level from json file
   }
-  std::vector<Object> levelObjects;
+  std::vector<std::unique_ptr<Actor>> walls;
+  std::vector<std::unique_ptr<Actor>> machines;
+  std::vector<std::unique_ptr<Actor>> objects;
 };
