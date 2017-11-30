d224abb1b5541192ce51d3ad3eb6c34caed2e552
jollyholly888 <31974419+jollyholly888@users.noreply.github.com>
2017-11-29 22:50:31 -0500

    Add files via upload
    headers for sound manager

---
+++ helloworld/include/Sounds.hpp
@@ -0,0 +1,18 @@
+#ifndef SOUNDS_H
+#define SOUNDS_H
+#include <SFML/Audio.hpp>
+public:
+  //each of these holds a file and sends the proper file to the bufferPlay which
+  //plays the proper file in the buffer.
+  void playMachine();
+  void playBuilder();
+  void playLauncher();
+  void playDoor();
+  void playHappy(); //beats a level, reunites with Kevin
+  void playSad(); //dies?? gets stuck?? is that possible??
+
+private:
+  int bufferPlay(float);
+
+
+#endif
