9c51c05dbd65dc4562dbfa869b22ca4c7646437c
aminnick <33011039+aminnick@users.noreply.github.com>
2017-12-05 02:10:45 -0500

    Implementing some sounds

--- helloworld/include/AudioManager.h
+++ helloworld/include/AudioManager.h
@@ -2,6 +2,7 @@
 #define AUDIO_MANAGER_H
 
 #include "GameView.h"
+#include <sstream>
 
 
 class AudioManager{
@@ -15,20 +16,21 @@ public:
 
 		sound.setBuffer(buffer);
 		sound.setVolume(50);
-		sound.setPitch(1.4);
+		sound.setPitch(2);
 
 		sound.play();
 	}
 
+// play sound for when
 	static void play_builder_box(){
 		printf("playing builder\n");
 		if(!buffer.loadFromFile("../include/Sounds/builder_box.wav")){
-			printf("no file\n");
+			printf("no sound file\n");
 		}
 
 
 		sound.setBuffer(buffer);
-		sound.setVolume(20);
+		sound.setVolume(10);
 		sound.setPitch(0.8);
 		sound.setPlayingOffset(sf::milliseconds(500));
 
@@ -37,6 +39,35 @@ public:
 		// sound.setVolume(1.2);
 		// sound.setPitch(1.2);
 	}
+
+// play music during the levels
+	static void play_level(int level){
+		music.stop();
+		std::stringstream lvlString;
+		lvlString << "../include/Sounds/level" << level << ".wav";
+		if(!music.openFromFile(lvlString.str())){
+			printf("no sound file\n");
+		}
+		music.setLoop(true);
+		music.play();
+	}
+
+// play exciting "congratulations!" music
+	static void play_end(){
+		music.stop();
+		if(!buffer.loadFromFile("../include/Sounds/coin_sound.wav")){
+			printf("no sound file\n");
+		}
+		sound.setBuffer(buffer);
+		sound.setPitch(1);
+		sound.setVolume(10);
+		sound.play();
+
+	}
+
+
+
+
 };
 
 
